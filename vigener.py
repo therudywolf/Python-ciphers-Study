a35418cb183c1d29eee0189c41b3371733864514
therudywolf <64037104+therudywolf@users.noreply.github.com>
2021-03-09 02:15:13 +0300

    Доделал матричный шифр

--- vigener.py
+++ vigener.py
@@ -1,6 +1,6 @@
 alphavite = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'  # алфавит
 alphavite2 = alphavite.upper()
-alphavite3 = ',.!:\'\"#?@[](){} '
+alphavite3 = ' '
 
 text = input('Введите сообщение \n')  # Ввод сообщения
 code = ''  # Зашифрованное сообщение
@@ -9,11 +9,14 @@ key = key + text
 krya = 0
 for i in text:  # блок шифрования
     if i.islower():
-        code += alphavite[(alphavite.find(i) + alphavite.find(key[krya])) % len(alphavite)]
+        code += alphavite[(alphavite.find(i) +
+                           alphavite.find(key[krya])) % len(alphavite)]
     elif i.isupper():
-        code += alphavite2[(alphavite2.find(i) + alphavite2.find(key[krya].upper())) % len(alphavite2)]
+        code += alphavite2[(alphavite2.find(i) +
+                            alphavite2.find(key[krya].upper())) % len(alphavite2)]
     else:
-        code += alphavite3[(alphavite3.find(i) + alphavite3.find(key[krya])) % len(alphavite3)]
+        code += alphavite3[(alphavite3.find(i) +
+                            alphavite3.find(key[krya])) % len(alphavite3)]
     krya = (krya + 1) % len(key)
 print(code)  # вывод зашифровнного текста
 
@@ -22,11 +25,14 @@ krya = 0
 
 for i in code:
     if i.islower():
-        decode += alphavite[(alphavite.find(i) - alphavite.find(key[krya])) % len(alphavite)]
+        decode += alphavite[(alphavite.find(i) -
+                             alphavite.find(key[krya])) % len(alphavite)]
     elif i.isupper():
-        decode += alphavite2[(alphavite2.find(i) - alphavite2.find(key[krya].upper())) % len(alphavite2)]
+        decode += alphavite2[(alphavite2.find(i) -
+                              alphavite2.find(key[krya].upper())) % len(alphavite2)]
     else:
-        decode += alphavite3[(alphavite3.find(i) - alphavite3.find(key[krya])) % len(alphavite3)]
+        decode += alphavite3[(alphavite3.find(i) -
+                              alphavite3.find(key[krya])) % len(alphavite3)]
     krya = (krya + 1) % len(key)
 
 print(decode)
